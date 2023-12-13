501e1fda21232d66892b6283af1bf39d67026cf6
Nathan-Simonnet <nathan.simonnet@gmail.com>
2023-12-13 15:35:20 +0100

    TP 3.1 done

--- TP3.1/index.js
+++ TP3.1/index.js
@@ -1,6 +1,28 @@
-console.log("kaamelot")
 // (https://github.com/A3lfyr/kaamelott-api)
 
-fetch('https://kaamelott.reiter.tf/quotes').then(response => response.json()).then(response => {
-    console.log(response);
+const here = document.getElementById('here');
+const quoteContainer = document.querySelector('.quote-container');
+
+const quoteDisplayer = function (data) {
+
+    const randomNumber = Math.floor(Math.random() * data.length)
+    console.log(randomNumber)
+    quoteContainer.innerHTML =
+        `
+    <p>Personnage: ${data[randomNumber].infos.personnage}</p>
+    <p>Citation: ${data[randomNumber].citation}</p>
+    
+    `
+}
+
+const quoteFetcher = function () {
+    fetch('https://kaamelott.reiter.tf/quotes')
+        .then(response => response.json())
+        .then(data => {
+            quoteDisplayer(data);
+        });
+}
+
+here.addEventListener('click', () => {
+    quoteFetcher();
 });
