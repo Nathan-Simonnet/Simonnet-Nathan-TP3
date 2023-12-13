c46d2fe1d28067722d9575f21b7b22711e55fdf6
Nathan-Simonnet <nathan.simonnet@gmail.com>
2023-12-13 16:58:30 +0100

    TP3.3 done

--- TP3.3/index.js
+++ TP3.3/index.js
@@ -1,26 +1,78 @@
-console.log("gouv")
-// [Documentation API](https://adresse.data.gouv.fr/api-doc/adresse)
-// [Géolocalisation](https://developer.mozilla.org/fr/docs/Web/API/Geolocation_API)
 
-fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
-    .then((response) => response.json())
-    .then((data) => console.log(data))
+const geoContainer = document.querySelector('.geo-container');
 
+// properties.label
 
+todoDisplayer = (data) => {
+    console.log(data)
+    geoContainer.innerHTML +=
+        `
+        <p>Ville: ${data[0].nom}</p>
+        <p>Population: ${data[0].population}</p>
+        <p>Code postaux: ${data[0].codesPostaux}</p>
+        <button id="lol">Click here for more</button>
+        `
+    document.getElementById('lol').addEventListener('click', () => {
+        document.getElementById('lol').textContent = "non"
+    });
+}
 
+adressDisplayer = (data) => {
+    console.log(data)
+    geoContainer.innerHTML +=
+        `
+        <p>Adresse: ${data.properties.label}</p>
+        `
+    console.log(data.properties.postcode)
+    todofetcher(data.properties.postcode)
 
+}
 
+todofetcher = (code) => {
+    fetch('https://geo.api.gouv.fr/communes?codePostal=' + code)
+        .then((response) => response.json())
+        .then((data) => todoDisplayer(data))
 
+}
 
+adressFetcher = (lat, lon) => {
+    fetch('https://api-adresse.data.gouv.fr/reverse/?lon=' + lon + '&lat=' + lat)
+        // fetch('https://api-adresse.data.gouv.fr/reverse/?lon=2.37&lat=48.357')
+        // fetch('https://api-adresse.data.gouv.fr/reverse/?lon=2.37&lat=48.357&type=street')
+        .then((response) => (response.json()))
+        .then((data) =>
+            adressDisplayer(data.features[0]))
 
+}
 
+geolocDispalyer = (Lattitude, longitude) => {
 
+    geoContainer.innerHTML =
+        `
+<p>Lattitude:  ${Lattitude}</p> 
+<p>longitude:  ${longitude}</p>
+`
+}
 
+function getUserLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
 
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    geolocDispalyer(latitude, longitude)
+    adressFetcher(latitude, longitude)
+    // todoFetcher(latitude, longitude)
+}
 
-
-
-
+document.getElementById('go').addEventListener('click', () => {
+    getUserLocation()
+});
 
 
 
